0d077225c9a2315b13e88b89bcf1e77ecf8b7471
jiaao <ao_jia@outlook.com>
2024-08-22 08:48:48 +0800

    fix: zoom in on images without refreshing the page.

--- docs/.vitepress/content.ts
+++ docs/.vitepress/content.ts
@@ -13,43 +13,27 @@ export const content: contentObject = {
       {
         text: 'Ubuntu',
         collapsed: false,
-        items: [
-          {
-            text: 'Install Ubuntu alongside Windows 10 (Dual Boot)',
-            link: 'dual-system-installation',
-          },
-          {
-            text: 'Quickstart with Ubuntu 22.04 Desktop',
-            link: 'quickstart-with-ubuntu',
-          },
-        ],
       },
       {
         text: 'Windows',
         collapsed: false,
-        items: [
-          {
-            text: 'Quickstart with Windows 10',
-            link: 'quickstart-with-windows',
-          },
-        ],
       },
       {
         text: 'Writer',
         collapsed: false,
-        items: [
-          {
-            text: 'VSCode + TexLive in Ubuntu2204 installed by WSL2',
-            link: 'vscode-texlive-wsl2',
-          },
-        ],
       },
       {
         text: 'Basic Skills',
         collapsed: false,
-        items: [{ text: 'Docker Notes', link: 'docker-notes' }],
       },
     ],
   },
-  ['LHY']: {},
+  ['Deep Learning']: {
+    sidebar: [
+      {
+        text: 'LHY',
+        collapsed: false,
+      },
+    ],
+  },
 }
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,14 +1,24 @@
 import DefaultTheme from 'vitepress/theme'
-import { onMounted } from 'vue'
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
 import mediumZoom from 'medium-zoom'
 
 import './index.css'
 
 export default {
   ...DefaultTheme,
+
   setup() {
-    onMounted(() => {
+    const route = useRoute()
+    const initZoom = () => {
       mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
+    }
+    onMounted(() => {
+      initZoom()
     })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
   },
 }
